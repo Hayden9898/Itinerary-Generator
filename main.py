19cbd8ce425a083e4d8eadef9442bf85f8c0aeef
Behrad Assa <behradassa@gmail.com>
2023-11-25 16:03:17 -0500

    Main works sometimes

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from openai import OpenAI
 import os
 from dotenv import load_dotenv, dotenv_values
+from taipy.gui import State, invoke_long_callback, notify
 
 load_dotenv()
 
@@ -59,73 +60,20 @@ scenario_cfg = Config.configure_scenario("scenario", task_configs=[build_msg_tas
 def get_days(state):
     return state.calc_trip_length(state.start_date, state.end_date)
 
-def get_num_Adults(state):
-    return state.num_adults
-
-def get_num_Kids(state):
-    return state.num_kids
-
-def verify_num_adults(num_adults):
-    # Initialize the verification flag to False
-    verification = False
-
-    # Continue the loop until verification is True
-    while not verification:
-        try:
-            # Attempt to convert the input to a float
-            num_adults = float(num_adults)
-
-            # Check if the input is a non-negative number
-            if num_adults >= 0:
-                # If valid, set verification to True and return the number of adults
-                verification = True
-                return num_adults
-            else:
-                # If not valid, return an error message
-                return "Please enter a non-negative number"
-        except ValueError:
-            # If the conversion to float fails, return an error message
-            return "Please enter a number"
-
-def verify_num_kids(num_kids):
-    # Initialize the verification flag to False
-    verification = False
-
-    # Continue the loop until verification is True
-    while not verification:
-        try:
-            # Attempt to convert the input to a float
-            num_kids = float(num_kids)
-
-            # Check if the input is a non-negative number
-            if num_kids >= 0:
-                # If valid, set verification to True and return the number of kids
-                verification = True
-                return num_kids
-            else:
-                # If not valid, return an error message
-                return "Please enter a non-negative number"
-        except ValueError:
-            # If the conversion to float fails, return an error message
-            return "Please enter a number"
- 
-def get_days(state):
-    return state.calc_trip_length(state.start_date, state.end_date)
-
-def gptPromptCreation():
-
-    return f"Create an itinerary for a trip to {Destination} for 1 days.\
-          There are {num_adults} adults and {num_kids} children going. Along with places to eat, and good photo taking opportunities. \
-          Please include times of day in the itinerary. Please include the hyperlinks to any relevant info (like restaurants) in the response. Do this in 100 words. "
+def gptPromptCreation(state):
+    return f"Create an itinerary for a trip to {state.Destination} for {calc_trip_length(state.start_date, state.end_date)} days.There are {state.num_adults} adults and {state.num_kids} children going. Please include times of day in the itinerary. Please include the hyperlinks to any relevant info (like restaurants) in the response. Also please restate my inputs. "
 
 
 def submit_scenario(state):
     
-    state.scenario.test_info.write(prompt(message=gptPromptCreation(),model="gpt-4-1106-preview"))
+    print(gptPromptCreation(state))
+
+    state.scenario.test_info.write(prompt(message=gptPromptCreation(state),model="gpt-4-1106-preview"))
 
     state.scenario.submit(wait=True)
 
     state.message = scenario.message.read()
+
 
 #Markdown representation of the UI
 
@@ -153,12 +101,11 @@ Message: <|{message}|text|>
 
 Destination = None
 message = None
-start_date = datetime.now()
-end_date = datetime.now()
-num_adults=2
-num_kids=2
+start_date = None
+end_date = None
+num_adults=None
+num_kids=None
 bool_pets=None
-
 
 if __name__ == "__main__":
     tp.Core().run()
